5eb3c9f7b5c29b64626bcac479d6a8bf829d245c
dave <dave@computesoftware.com>
2018-06-20 12:00:53 -0700

    Use deps.edn, update franzy ref

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject timbre-kafka-appender "0.0.2"
+(defproject timbre-kafka-appender "0.0.3"
   :description "timbre-kafka-appender is an appender
  for logging into kafka server"
-  :url "https://github.com/chao787/timbre-kafka-appender"
+  :url "https://github.com/ComputeSoftware/timbre-kafka-appender"
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/mit-license.php"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [ymilky/franzy       "0.0.1"
-                  :exclusions [org.clojure/clojure]]])
+  :plugins [[lein-tools-deps "0.4.1"]]
+  :middleware [lein-tools-deps.plugin/resolve-dependencies-with-deps-edn]
+  :lein-tools-deps/config {:config-files [:install :user :project]})
